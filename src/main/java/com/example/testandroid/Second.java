b934d37d547dbdbb0d11e3ca3c21183a9c4595d4
Snigdha Kashyap <shivi2001sn@gmail.com>
2019-06-09 20:19:20 +0530

    Update Second.java

--- src/main/java/com/example/testandroid/Second.java
+++ src/main/java/com/example/testandroid/Second.java
@@ -1,3 +1,4 @@
+//Relative layout
 package com.example.testandroid;
 
 import android.content.Intent;
@@ -5,33 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.Serializable;
 
 public class Second extends AppCompatActivity {
-    TextView txt,txt2;
     Button b2;
-    private Third objThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        objThird=new Third();
         setContentView(R.layout.activity_second);
-        txt=(TextView)findViewById(R.id.txt);
-        txt2=(TextView)findViewById(R.id.txt2);
         b2=(Button)findViewById(R.id.b2);
         Bundle extras=getIntent().getExtras();
-        final Third objThird=(Third)getIntent().getExtras().get("Third");
-        txt.setText("Snigdha Kashyap");
-        txt2.setText("Khushi Gupta");
         b2.setOnClickListener(new View.OnClickListener() {
                                   @Override
                                   public void onClick(View v) {
 
                                       Intent i=new Intent( Second.this,Third.class);
-                                      i.putExtra("Third", (Serializable) objThird);
                                       startActivity(i);
                                   }
                               }
